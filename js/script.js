3a84137b38a7ba8b769047059400359065dedbea
Joan <kkasemiire711@gmail.com>
2024-01-27 17:48:41 +0300

    Added closeModal and openModal functions

--- js/script.js
+++ js/script.js
@@ -2,27 +2,36 @@ var button = document.querySelector(".got-this");
 var modalDiv = document.querySelector(".modal");
 var modalX = document.querySelector(".modal-x");
 
-button.addEventListener("click", function(){
-    modalDiv.classList.add("show-modal");
-    button.innerText = "You got this?"
-    
+var openModel = function () {
+  modalDiv.classList.add("show-modal");
+  button.innerText = "You got this?";
+};
+
+button.addEventListener("click", function () {
+  openModel();
+  // modalDiv.classList.add("show-modal");
+  // button.innerText = "You got this?";
 });
 
-modalX.addEventListener("click", function(){
-  modalDiv.classList.remove("show-modal")
-  button.innerText = "Who's got this?"
+var closeModal = function () {
+  modalDiv.classList.remove("show-modal");
+  button.innerText = "Who's got this?";
+};
+
+modalX.addEventListener("click", function () {
+  closeModal();
 });
 
-document.addEventListener("keydown", function(e){
-  if (e.key === "Escape"){
-    if (modalDiv.classList.contains("show-modal")){
-      modalDiv.classList.remove("show-modal")
-      button.innerText = "Who's got this?"
-    } 
-    else {
-      modalDiv.classList.add("show-modal");
-      button.innerText = "You got this?"
-    };
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape") {
+    if (modalDiv.classList.contains("show-modal")) {
+      closeModal();
+      // modalDiv.classList.remove("show-modal")
+      // button.innerText = "Who's got this?"
+    } else {
+      openModel();
+      // modalDiv.classList.add("show-modal");
+      // button.innerText = "You got this?";
+    }
   }
-})
-
+});
