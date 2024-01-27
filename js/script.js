aa531d135cb52b429083eaf861e6a6c702476454
Joan <kkasemiire711@gmail.com>
2024-01-27 17:24:23 +0300

    Added a keydown event in the js file

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ var modalX = document.querySelector(".modal-x");
 
 button.addEventListener("click", function(){
     modalDiv.classList.add("show-modal");
-    button.innerText = "Who's got this?"
+    button.innerText = "You got this?"
     
 });
 
@@ -12,3 +12,17 @@ modalX.addEventListener("click", function(){
   modalDiv.classList.remove("show-modal")
   button.innerText = "Who's got this?"
 });
+
+document.addEventListener("keydown", function(e){
+  if (e.key === "Escape"){
+    if (modalDiv.classList.contains("show-modal")){
+      modalDiv.classList.remove("show-modal")
+      button.innerText = "Who's got this?"
+    } 
+    else {
+      modalDiv.classList.add("show-modal");
+      button.innerText = "You got this?"
+    };
+  }
+})
+
